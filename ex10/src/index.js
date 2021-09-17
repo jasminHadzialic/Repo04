b549bacdeea4afc543991595e745e65971185c0d
Jasmin Hadzialic <46654927+jasminHadzialic@users.noreply.github.com>
2021-09-17 09:15:56 +0200

    declare string

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main () {
+    // Only change code below this line
+    var myFirstName = "Jasmin";
+    var myLastName = "Hadzialic";
+    var myHomeTown = "Travnik";
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+
+module.exports = main;
